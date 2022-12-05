f8b37280066de672952aede6a05efddc69373a34
Mark <pivovarov.marck@yandex.ru>
2022-12-05 15:52:17 +0300

    lab3

--- src/main/java/lab3/MyArraySizeException.java
+++ src/main/java/lab3/MyArraySizeException.java
@@ -1,2 +1,9 @@
-package lab3;public class MyArraySizeException {
+package lab3;
+
+public class MyArraySizeException extends Exception
+{
+    public MyArraySizeException(String message)
+    {
+        super(message);
+    }
 }
